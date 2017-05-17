3bf75aa1f82d7a36bf6f0ea1dd0542e31e15dfe3
Animesh Sharma <anime.mit5@gmail.com>
2017-05-17 11:53:24 +0530

    Computes SHA-256 checksum of all files in selected folder and stores it as checksum.sha

--- app/src/main/java/com/skylar/foldersync/MainActivity.java
+++ app/src/main/java/com/skylar/foldersync/MainActivity.java
@@ -1,57 +1,104 @@
 package com.skylar.foldersync;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final int REQUEST_WRITE_PERMISSION = 1;
+    private boolean isChosen = false;
+    private String dir = "";
 
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        }// else onDirSel();
 
-    public String computeHash(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-
-        MessageDigest digest = MessageDigest.getInstance("SHA-512");
-        digest.reset();
-        byte[] byteData = digest.digest(input.getBytes("UTF-8"));
-
-        StringBuffer sb = new StringBuffer();
-
-        for (int i = 0; i < byteData.length; i++) {
-            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-        }
-        return sb.toString();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            onDirSel();
+        else requestPermission();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission();
+    }
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+    public void onDirSel() {
+        final Button dirChooserButton = (Button) findViewById(R.id.button);
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        dirChooserButton.setOnClickListener(new OnClickListener() {
+            private String m_chosenDir = "";
+            private boolean m_newFolderEnabled = true;
+
+
+            @Override
             public void onClick(View v) {
-                EditText et = (EditText) findViewById(R.id.editText1);
-                String in = et.getText().toString();
-                TextView tv = (TextView) findViewById(R.id.textView2);
-                try {
-                    tv.setText(computeHash(in));
-                } catch (NoSuchAlgorithmException x) {
-                    Log.i("Error", "No such Algorithm");
-                } catch (UnsupportedEncodingException x) {
-                    Log.i("Error", "Unsupported Encoding");
-                }
+                /*if (isChosen) {
+                    Intent myIntent = new Intent(MainActivity.this, CryptActivity.class);
+                    myIntent.putExtra("ChosenDirectory", m_chosenDir);
+                    startActivity(myIntent);
+
+                }*/
+                // Create DirectoryChooserDialog and register a callback
+                DirectoryChooserDialog directoryChooserDialog =
+                        new DirectoryChooserDialog(MainActivity.this,
+                                new DirectoryChooserDialog.ChosenDirectoryListener() {
+                                    @Override
+                                    public void onChosenDir(String chosenDir) {
+                                        m_chosenDir = chosenDir;
+                                        dir = chosenDir;
+                                        Toast.makeText(
+                                                MainActivity.this, "Chosen directory: " +
+                                                        chosenDir, Toast.LENGTH_LONG).show();
+                                        tv.setText(chosenDir);
+                                        //dirChooserButton.setText(getString(R.string.compute_button));
+                                        isChosen = true;
+
+                                        //finish();
+                                    }
+                                });
+                // Toggle new folder button enabling
+                directoryChooserDialog.setNewFolderEnabled(m_newFolderEnabled);
+                // Load directory chooser dialog for initial 'm_chosenDir' directory.
+                // The registered callback will be called upon final directory selection.
+                directoryChooserDialog.chooseDirectory(m_chosenDir);
+                m_newFolderEnabled = !m_newFolderEnabled;
+
             }
+
         });
 
 
+        final Button computeButton = (Button) findViewById(R.id.button2);
+        computeButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isChosen) {
+                    Intent myIntent = new Intent(MainActivity.this, CryptActivity.class);
+                    myIntent.putExtra("ChosenDirectory", dir);
+                    startActivity(myIntent);
+                }
+                else Toast.makeText(MainActivity.this, "No Directory Chosen!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
+
+
+
